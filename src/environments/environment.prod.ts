c8d93dac5997cc2d22b77bdd39337a792a4cd3a4
Michaël Cloots <michael.cloots@thomasmore.be>
2020-10-03 14:44:17 +0200

    firebase deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  cloudName: 'pwaws',
+  uploadPreset: 'rmfqo2i9',
+  firebase: {
+    apiKey: "AIzaSyAuYKDsM1VLjJpKSNPA-MzOG9tI5vbrIME",
+    authDomain: "pwa-workshop-86b4c.firebaseapp.com",
+    databaseURL: "https://pwa-workshop-86b4c.firebaseio.com",
+    projectId: "pwa-workshop-86b4c",
+    storageBucket: "pwa-workshop-86b4c.appspot.com",
+    messagingSenderId: "32091744200",
+    appId: "1:32091744200:web:6af27c6bbef6fc620602fd"
+  }
 };
